0a9490cf29ceb0b42771e23ddc373c83f5d45edc
mauruco <mauruco@gmail.com>
2020-04-24 10:37:53 -0300

    multiple types

--- src/request.js
+++ src/request.js
@@ -1,7 +1,12 @@
 const https = require('https');
+const http = require('http');
+const qs = require('querystring');
 
-const request = (options, postData) => new Promise((resolve, reject) => {
-  const req = https.request(options, (res) => {
+module.exports = (type, options, postData) => new Promise((resolve, reject) => {
+
+  const httpOrHttps = type === 'http' ? http : https;
+  
+  const req = httpOrHttps.request(options, (res) => {
     res.setEncoding('utf8');
     let body = '';
     res.on('data', (chunk) => {
@@ -25,15 +30,21 @@ const request = (options, postData) => new Promise((resolve, reject) => {
         const result = body.replace(/:([\d]{13,})([,|}|\s]+)/g, ':"$1"$2');
         return resolve({ statusCode, body: JSON.parse(result) });
       } catch (error) {
-        console.log('services/request -> RESPONSE IS NOT JSON', error);
         return resolve({ statusCode, body });
       }
     });
   });
 
   req.on('error', (e) => reject(e));
-  if (postData && (options.method === 'POST' || options.method === 'PUT')) req.write(postData);
+  if (postData && (options.method === 'POST' || options.method === 'PUT')) {
+    const headersType = JSON.stringify(options.headers);
+    if (headersType.match(/content-type"\s?:\s?"application\/json/gi)){
+      req.write(JSON.stringify(postData));
+    } else if (headersType.match(/content-type"\s?:\s?"application\/x-www-form-urlencoded/gi)) {
+      req.write(qs.stringify(postData));
+    } else {
+      req.write(postData);
+    }
+  }
   req.end();
 });
-
-module.exports = request;
